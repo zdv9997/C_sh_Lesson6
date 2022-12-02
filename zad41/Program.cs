b2759bd98db1f287cba8d14250ac455ca226be8b
Dmitriy <zdv9997@mail.ru>
2022-12-02 11:38:46 +0200

    считаем количество чисел больше нуля

---
+++ zad41/Program.cs
@@ -0,0 +1,12 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Write("Какое количество чисел будем вводить?:");
+int m = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int[] array = new int[m];
+Console.WriteLine("Введите числа:");
+for (int i = 0; i < array.Length; i++) array[i] = Convert.ToInt32(Console.ReadLine());
+foreach (var i in array)
+{
+    if (i > 0) count++;
+}
+Console.WriteLine($"Количество чисел больше нуля: {count}");
